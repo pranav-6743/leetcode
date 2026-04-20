9da8385f59bded22677d476bb91fd6678c1ae6cb
pranav-6743 <prakashnaveen587@gmail.com>
2026-04-20 14:26:44 +0530

    Time: 2 ms (97.95%) | Memory: 43.2 MB (60.38%) - LeetSync

---
+++ 20-valid-parentheses/valid-parentheses.java
@@ -0,0 +1,27 @@
+import java.util.Stack;
+
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        
+        for (char c : s.toCharArray()) {
+            // If it's an opening bracket, push the corresponding closing bracket
+            if (c == '(') {
+                stack.push(')');
+            } else if (c == '{') {
+                stack.push('}');
+            } else if (c == '[') {
+                stack.push(']');
+            } 
+            // If it's a closing bracket, check if it matches the top of the stack
+            else {
+                if (stack.isEmpty() || stack.pop() != c) {
+                    return false;
+                }
+            }
+        }
+        
+        // If the stack is empty, all brackets were matched correctly
+        return stack.isEmpty();
+    }
+}
