3d147af979815f9f8a8d186298d3f95066b8645a
pranav-6743 <prakashnaveen587@gmail.com>
2026-04-20 14:39:38 +0530

    Time: 383 ms (15.96%) | Memory: 42.4 MB (98.15%) - LeetSync

---
+++ 37-sudoku-solver/sudoku-solver.java
@@ -0,0 +1,40 @@
+class Solution {
+    public void solveSudoku(char[][] board) {
+        solve(board);
+    }
+
+    private boolean solve(char[][] board) {
+        for (int row = 0; row < 9; row++) {
+            for (int col = 0; col < 9; col++) {
+                // Find an empty cell
+                if (board[row][col] == '.') {
+                    for (char c = '1'; c <= '9'; c++) {
+                        if (isValid(board, row, col, c)) {
+                            board[row][col] = c; // Try placing the digit
+
+                            if (solve(board)) {
+                                return true; // If it leads to a solution, return true
+                            } else {
+                                board[row][col] = '.'; // Otherwise, backtrack
+                            }
+                        }
+                    }
+                    return false; // No digit from 1-9 works here
+                }
+            }
+        }
+        return true; // All cells filled
+    }
+
+    private boolean isValid(char[][] board, int row, int col, char c) {
+        for (int i = 0; i < 9; i++) {
+            // Check row
+            if (board[i][col] == c) return false;
+            // Check column
+            if (board[row][i] == c) return false;
+            // Check 3x3 sub-box
+            if (board[3 * (row / 3) + i / 3][3 * (col / 3) + i % 3] == c) return false;
+        }
+        return true;
+    }
+}
