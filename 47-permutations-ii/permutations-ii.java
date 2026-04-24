7495bfea60adc584469406fb8d63651f28cc1d00
pranav-6743 <prakashnaveen587@gmail.com>
2026-04-24 13:03:01 +0530

    Time: 3 ms (88.46%) | Memory: 46.8 MB (48.65%) - LeetSync

--- 47-permutations-ii/permutations-ii.java
+++ 47-permutations-ii/permutations-ii.java
@@ -1,40 +1,34 @@
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.List;
+import java.util.*;
 
 class Solution {
+
     public List<List<Integer>> permuteUnique(int[] nums) {
         List<List<Integer>> result = new ArrayList<>();
-        // 1. Sort to keep duplicate elements together
         Arrays.sort(nums);
         backtrack(result, new ArrayList<>(), nums, new boolean[nums.length]);
         return result;
     }
 
-    private void backtrack(List<List<Integer>> result, List<Integer> current, int[] nums, boolean[] used) {
-        if (current.size() == nums.length) {
-            result.add(new ArrayList<>(current));
+    private void backtrack(List<List<Integer>> result, List<Integer> temp, int[] nums, boolean[] used) {
+        if (temp.size() == nums.length) {
+            result.add(new ArrayList<>(temp));
             return;
         }
 
         for (int i = 0; i < nums.length; i++) {
-            // Skip if the element at this index is already used
+
             if (used[i]) continue;
 
-            // 2. Skip duplicates:
-            // If current element is the same as previous, and the previous 
-            // has NOT been used in the current path, skip this one to avoid 
-            // generating the same permutation twice.
+            // skip duplicates
             if (i > 0 && nums[i] == nums[i - 1] && !used[i - 1]) continue;
 
             used[i] = true;
-            current.add(nums[i]);
-            
-            backtrack(result, current, nums, used);
-            
-            // Backtrack
+            temp.add(nums[i]);
+
+            backtrack(result, temp, nums, used);
+
+            temp.remove(temp.size() - 1);
             used[i] = false;
-            current.remove(current.size() - 1);
         }
     }
 }
