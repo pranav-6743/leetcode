a4bc4c0ab1adc509a9412c519db905fc8fcd3007
pranav-6743 <prakashnaveen587@gmail.com>
2026-04-24 13:19:59 +0530

    Time: 8 ms (91.23%) | Memory: 49.2 MB (41.75%) - LeetSync

---
+++ 56-merge-intervals/merge-intervals.java
@@ -0,0 +1,23 @@
+import java.util.*;
+
+class Solution {
+    public int[][] merge(int[][] intervals) {
+        Arrays.sort(intervals, (a, b) -> a[0] - b[0]);
+
+        List<int[]> result = new ArrayList<>();
+
+        int[] current = intervals[0];
+        result.add(current);
+
+        for (int[] next : intervals) {
+            if (next[0] <= current[1]) {
+                current[1] = Math.max(current[1], next[1]);
+            } else {
+                current = next;
+                result.add(current);
+            }
+        }
+
+        return result.toArray(new int[result.size()][]);
+    }
+}
